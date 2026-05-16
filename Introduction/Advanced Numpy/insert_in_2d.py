affa807e0d93c2a1bd8f279136f3d824c480abf0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-16 12:17:56 +0600

    Here we started before going to class

---
+++ Introduction/Advanced Numpy/insert_in_2d.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr_2d = np.array([[1,2],[3,4]])
+print(arr_2d)
+print("\n\n")
+# Insert a new row at index 1
+new_arr_2d = np.insert(arr_2d, 1 , [5,6], axis = None)
+print(new_arr_2d)
